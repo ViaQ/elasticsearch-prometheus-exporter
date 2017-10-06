725ae80d33fe8010251d628b1577b2c6a2e1d4c7
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-10-06 15:51:16 +0200

    Pull both metrics in async manner

--- src/main/java/org/elasticsearch/action/TransportNodePrometheusMetricsAction.java
+++ src/main/java/org/elasticsearch/action/TransportNodePrometheusMetricsAction.java
@@ -1,7 +1,13 @@
 package org.elasticsearch.action;
 
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.action.admin.cluster.node.stats.NodesStatsRequest;
+import org.elasticsearch.action.admin.cluster.node.stats.NodesStatsResponse;
 import org.elasticsearch.action.support.ActionFilters;
 import org.elasticsearch.action.support.HandledTransportAction;
+import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.ClusterService;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
@@ -12,18 +18,16 @@ import org.elasticsearch.transport.TransportService;
 
 public class TransportNodePrometheusMetricsAction extends HandledTransportAction<NodePrometheusMetricsRequest, NodePrometheusMetricsResponse> {
 
-//    private final Client client;
-
     protected final ClusterName clusterName;
     protected final ClusterService clusterService;
+    protected final Client client;
 
     @Inject
-    public TransportNodePrometheusMetricsAction(Settings settings, ClusterName clusterName, ThreadPool threadPool,
-                                            /*Client client,*/
+    public TransportNodePrometheusMetricsAction(Settings settings, ClusterName clusterName, ThreadPool threadPool, Client client,
                                             ClusterService clusterService, TransportService transportService, ActionFilters actionFilters,
                                                 IndexNameExpressionResolver indexNameExpressionResolver) {
         super(settings, NodePrometheusMetricsAction.NAME, threadPool, transportService, actionFilters, indexNameExpressionResolver, NodePrometheusMetricsRequest.class);
-//        this.client = client;
+        this.client = client;
         this.clusterName = clusterName;
         this.clusterService = clusterService;
     }
@@ -37,10 +41,16 @@ public class TransportNodePrometheusMetricsAction extends HandledTransportAction
 
         private final NodePrometheusMetricsRequest request;
         private final ActionListener<NodePrometheusMetricsResponse> listener;
+        private final ClusterHealthRequest healthRequest;
+        private final NodesStatsRequest nodesStatsRequest;
+        private ClusterHealthResponse clusterHealthResponse;
 
         private AsyncAction(NodePrometheusMetricsRequest request, ActionListener<NodePrometheusMetricsResponse> listener) {
             this.request = request;
             this.listener = listener;
+            this.healthRequest = new ClusterHealthRequest();
+            this.nodesStatsRequest = new NodesStatsRequest("_local").all();
+//            this.nodesStatsRequest = new NodesStatsRequest("_local").clear();
         }
 
         private void start() {
@@ -48,13 +58,49 @@ public class TransportNodePrometheusMetricsAction extends HandledTransportAction
                 logger.info("Start action");
                 logger.info("Cluster name: [{}], Node name: [{}]", clusterName.toString(), clusterService.localNode().name());
 //            }
-            listener.onResponse(buildResponse());
+
+            client.admin().cluster().health(healthRequest, clusterHealthResponseActionListener);
         }
 
-        protected NodePrometheusMetricsResponse buildResponse() {
+        private ActionListener<NodesStatsResponse> nodesStatsResponseActionListener = new ActionListener<NodesStatsResponse>() {
+            @Override
+            public void onResponse(NodesStatsResponse nodeStats) {
+                listener.onResponse(buildResponse(clusterHealthResponse, nodeStats));
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                listener.onFailure(new ElasticsearchException("Nodes stats request failed"));
+            }
+        };
+
+        private ActionListener<ClusterHealthResponse> clusterHealthResponseActionListener = new ActionListener<ClusterHealthResponse>() {
+            @Override
+            public void onResponse(ClusterHealthResponse response) {
+                clusterHealthResponse = response;
+                client.admin().cluster().nodesStats(nodesStatsRequest, nodesStatsResponseActionListener);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                listener.onFailure(new ElasticsearchException("Cluster health request failed"));
+            }
+        };
+
+        protected String buildClusterHealthResponse(ClusterHealthResponse response) {
+            return response.toString();
+        }
+
+        protected String buildNodesStatsResponse(NodesStatsResponse response) {
+            return response.toString();
+        }
+
+        protected NodePrometheusMetricsResponse buildResponse(ClusterHealthResponse clusterHealth, NodesStatsResponse nodesStats) {
 //            if (logger.isTraceEnabled()) {
-                logger.info("Return response");
+                logger.info("Return response:");
                 logger.info("Cluster name: [{}], Node name: [{}]", clusterName.toString(), clusterService.localNode().name());
+                logger.info("Cluster health [{}]", buildClusterHealthResponse(clusterHealth));
+                logger.info("Nodes Stats [{}]", buildNodesStatsResponse(nodesStats));
 //            }
             return new NodePrometheusMetricsResponse();
         }
